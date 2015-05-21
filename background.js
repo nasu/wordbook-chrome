0c587aa294ef11544f1762d3f6cd8b3bb58e758f
nasu <tatu.nasu@gmail.com>
2015-05-22 01:05:39 +0900

    Add config

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 var Dict = (function () {
     var Dict = function () {};
     var p = Dict.prototype;
+    p.SAVE_URL   = "";
+    p.GET_URL    = "";
+    p.SEARCH_URL = "";
     p.search = function (word) {
         var that = this;
-        $.get('http://public.dejizo.jp/NetDicV09.asmx/SearchDicItemLite?' +
-            'Dic=EJdict&Word=' + word + '&' +
-            'Scope=HEADWORD&Match=STARTWITH&Merge=AND&' +
-            'Prof=XHTML&PageSize=1&PageIndex=0',
+        $.get(this.SEARCH_URL + word,
             function (res) {
                 $(res).find('ItemID').each(function (i, v) {
                     that.get(v);
@@ -15,9 +15,7 @@ var Dict = (function () {
         );
     };
     p.get = function (itemID) {
-        $.get('http://public.dejizo.jp/NetDicV09.asmx/GetDicItemLite?' +
-            'Dic=EJdict&Item=' + $(itemID).text() + '&' +
-            'Loc=&Prof=XHTML',
+        $.get(this.GET_URL + $(itemID).text(),
             function (res) {
                 $(res).find('.NetDicBody').each(function (i, v) {
                     alert($(v).text());
@@ -25,15 +23,33 @@ var Dict = (function () {
             }
         );
     };
+    p.save = function (word) {
+        $.post(this.SAVE_URL + word, function (res) {});
+    };
     return Dict;
 })();
+chrome.runtime.getPackageDirectoryEntry(function (root) {
+    root.getFile('config.json', {}, function (entry) {
+        entry.file(function (file) {
+            var reader = new FileReader();
+            reader.onloadend = function (e) {
+                var config = JSON.parse(this.result);
+                var p = Dict.prototype;
+                p.SAVE_URL   = config.save_url;
+                p.GET_URL    = config.get_url;
+                p.SEARCH_URL = config.search_url;
+            };
+            reader.readAsText(file);
+        });
+    });
+});
 chrome.contextMenus.create({
     title: "「%s」をWordbookに登録",
     type: "normal",
     contexts: ["selection"],
     onclick: function (info, tab) {
-        $.post('http://localhost:8000/word/' + info.selectionText, function (res) {});
         var dict = new Dict();
+        dict.save(info.selectionText);
         dict.search(info.selectionText);
     }
 });
